0f716826d0d9e602cb81c151c777f940785ac866
Arnaud <arnaud.salomon20@gmail.com>
2019-04-18 18:49:39 +0200

    Arnaud

--- app.js
+++ app.js
@@ -1,46 +1,48 @@
-var createError = require('http-errors');
 var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var getNotesRouter = require('./routes/getNotes');
-var deleteNotesRouter = require('./routes/deleteNotes');
-var putNotesRouter = require('./routes/putNotes');
-var signInRouter = require('./routes/signIn');
-var signUpRouter = require('./routes/signUp');
+var session = require('cookie-session'); // Charge le middleware de sessions
+var bodyParser = require('body-parser'); // Charge le middleware de gestion des paramètres
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hjs');
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+/* On utilise les sessions */
+app.use(session({secret: 'todotopsecret'}))
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+/* S'il n'y a pas de todolist dans la session,
+on en crée une vide sous forme d'array avant la suite */
+.use(function(req, res, next){
+    if (typeof(req.session.todolist) == 'undefined') {
+        req.session.todolist = [];
+    }
+    next();
+})
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+/* On affiche la todolist et le formulaire */
+.get('/todo', function(req, res) { 
+    res.render('todo.ejs', {todolist: req.session.todolist});
+})
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+/* On ajoute un élément à la todolist */
+.post('/todo/ajouter/', urlencodedParser, function(req, res) {
+    if (req.body.newtodo != '') {
+        req.session.todolist.push(req.body.newtodo);
+    }
+    res.redirect('/todo');
+})
 
-module.exports = app;
+/* Supprime un élément de la todolist */
+.get('/todo/supprimer/:id', function(req, res) {
+    if (req.params.id != '') {
+        req.session.todolist.splice(req.params.id, 1);
+    }
+    res.redirect('/todo');
+})
+
+/* On redirige vers la todolist si la page demandée n'est pas trouvée */
+.use(function(req, res, next){
+    res.redirect('/todo');
+})
+
+.listen(8080);   
