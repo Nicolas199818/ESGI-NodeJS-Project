45d2c4e7653f7085590807202868eb7f75f98a52
thibault <nthibault@myges.fr>
2019-04-18 14:15:46 +0200

    Première version connexion

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var signupRouter = require('./routes/signup');
+var signinRouter = require('./routes/signin');
 
 var app = express();
 
@@ -21,6 +23,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/signup',signupRouter);
+app.use('/signin',signinRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -39,3 +43,8 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
+
+//Etape 1 : Avoir une base de données dans laquel taper afin de pouvoir réaliser les tests.
+  //Objectif 1 : Se connecter à la base de donnée.
+  //Objectif 2 : Réussir lorsque tu te connecte à l'url /signup à créer un user.
+  //Objectif 3 : Réussir lorsque tu te connecte à l'URL /signin à vérifier dans la base.
---
+++ routes/signin.js
@@ -0,0 +1,68 @@
+var express = require('express');
+var router = express.Router();
+
+router.post('/',function(req,res,next){
+  //res.render('signup', { title: 'Notes-Pages' });
+  if(req.body.password.length < 4){
+    res.status(400).send({error: 'Le mot de passe doit contenir au moins 4 caractères'});
+  }
+  else if(!checkUser(req.body.user)){
+    res.status(400).send({error: 'Votre identifiant ne doit contenir que des lettres minuscules non accentuées'});
+  }
+  else if(req.body.user.length <2 || req.body.user.length > 20){
+    res.status(400).send({error: 'Votre identifiant doit contenir entre 2 et 20 caractères'})
+  }
+  else{
+    signupFunction(req.body.user,req.body.password,res);
+  }
+
+
+});
+
+module.exports = router;
+
+function signupFunction(user,password,res){
+  const MongoClient = require('mongodb').MongoClient;
+  const assert = require('assert');
+  // Connection URL
+  const url = 'mongodb://localhost:27017/notes-api';
+  // Database Name
+  const dbName = 'notes-api';
+
+  (async function() {
+
+  const client = new MongoClient(url);
+
+  try {
+    // Use connect method to connect to the Server
+    await client.connect();
+    const db = client.db(dbName);
+    //On stocke d'abord la connexion.
+    //On check si le nom existe dans la base.
+    //Si oui erreur. Si non on insère.
+
+    var myobj = { username: user, password: password };
+    var myCol = db.collection("users");
+    if(!await myCol.findOne({username:user})){
+      res.status(400).send({error: 'Cet identifiant est inconnu'})
+    }
+    else{
+      res.render('signup');
+    }
+  } catch (err) {
+    console.log(err.stack);
+  }
+
+  client.close();
+})();
+}
+
+//On écris cette fonction pour checker que le mot est écris de lettre en a-z.
+function checkUser(user){
+  if(/^[a-z]+$/.test(user)){
+      return true;
+  }
+  else {
+     return false;
+  }
+}
